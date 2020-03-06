b08dd54eeb2725a8723c83b823616616ec32b7a7
Patrick Kille <pr.kille@gmail.com>
2020-03-06 13:55:22 -0800

    add in styling and extra flair for user

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -7,34 +7,35 @@ namespace Counting
   {
     public static void Main()
     {
-     Console.WriteLine("Please enter a sentence:");
-     string sentence = Console.ReadLine();
-     Console.WriteLine("Which word would you like to count?:");
-     string word = Console.ReadLine();
-     try
-     {
-      if (RepeatCounter.IsWord(word))
+      Console.WriteLine("Do you need to know how many times you wrote a single word?\nIt's your lucky day!\nIf you enter a sentence I will happily count the number of times a word is in it.");
+      Console.WriteLine("Please enter a sentence:");
+      string sentence = Console.ReadLine();
+      Console.WriteLine("Which word would you like me to count?:");
+      string word = Console.ReadLine();
+      try
       {
-        int count = RepeatCounter.WordCount(word, sentence);
-        Console.WriteLine($"That word appears {count} times.");
+        if (RepeatCounter.IsWord(word))
+        {
+          int count = RepeatCounter.WordCount(word, sentence);
+          Console.WriteLine($"That word appears {count} times.");
+        }
+        else
+        {
+          throw new System.InvalidOperationException("SassybotERROR 999 : Do you not know what a single word looks like?");
+        }
       }
-      else
+      catch (Exception ex)
       {
-        throw new System.InvalidOperationException("SassybotERROR 999 : Do you not know what a single word looks like?");
-      }
-     }
-     catch (Exception ex)
-     {
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Message = {0}", ex.Message);
         Console.WriteLine("Source = {0}", ex.Source);
         Console.WriteLine("StackTrace = {0}", ex.StackTrace);
         Console.WriteLine("TargetSite = {0}", ex.TargetSite);
-
+        Console.ResetColor();
         Console.WriteLine("\nPress Enter to start over.");
         Console.ReadLine();
         Main();
-     }
+      }
     }
   }
-
 }
