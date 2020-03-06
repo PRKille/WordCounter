ebdc11a590e048fe4d4871113e40288eb98fc7ed
Patrick Kille <pr.kille@gmail.com>
2020-03-06 15:18:00 -0800

    add the ability to restart after successful count operation.

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -17,7 +17,40 @@ namespace Counting
         if (RepeatCounter.IsWord(word))
         {
           int count = RepeatCounter.WordCount(word, sentence);
-          Console.WriteLine($"That word appears {count} times.");
+          Console.WriteLine($"That word appears {count} times.\n\nWould you like to search again y/n?");
+          string again = Console.ReadLine().ToLower();
+          try
+          {
+            switch (again)
+            {
+              case "y":
+                Main();
+                break;
+              case "n":
+                Console.Clear();
+                Console.WriteLine(@"
+               ______  _____   _____  ______  ______  __   __ _______
+              |  ____ |     | |     | |     \ |_____]   \_/   |______
+              |_____| |_____| |_____| |_____/ |_____]    |    |______
+                                                                      
+          ");
+                break;
+              default:
+                throw new System.InvalidOperationException("ERROR 4389 : No Banana Stickers were found. y = yes, n for no ya silly monkey!");
+            }
+          }
+          catch (Exception ex)
+          {
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine("Message = {0}", ex.Message);
+          Console.WriteLine("Source = {0}", ex.Source);
+          Console.WriteLine("StackTrace = {0}", ex.StackTrace);
+          Console.WriteLine("TargetSite = {0}", ex.TargetSite);
+          Console.ResetColor();
+          Console.WriteLine("\nPress Enter to start over.");
+          Console.ReadLine();
+          Main();
+          }         
         }
         else
         {
